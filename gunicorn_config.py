80934e8ab5d4295244c35d67b484f385dbbda0b6
nolancoe <99825787+nolancoe@users.noreply.github.com>
2024-01-24 17:52:36 -0600

    gunicorn config stuff

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 workers = 4  # Adjust based on server resources
 bind = "45.33.29.74:8000"
-chdir = "/var/www/DjangoApp/sweatygames"
+chdir = "/var/www/DjangoApp/"
 module = "sweatygames.wsgi:application"
