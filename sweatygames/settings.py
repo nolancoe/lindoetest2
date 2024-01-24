64250c97ddf529f1a9db413553ecdb1c01f22485
nolancoe <99825787+nolancoe@users.noreply.github.com>
2024-01-23 22:07:34 -0600

    debug false

--- sweatygames/settings.py
+++ sweatygames/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django_secret'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '45.33.29.74', 'sweatygames.pro']
 
