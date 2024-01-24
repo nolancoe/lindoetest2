265402c289c4a44930738abd2e7e53e1ebc9e91b
nolancoe <99825787+nolancoe@users.noreply.github.com>
2024-01-24 15:58:39 -0600

    staticy

--- sweatygames/settings.py
+++ sweatygames/settings.py
@@ -226,9 +226,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media'),  # Add your static files directory here
-]
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
