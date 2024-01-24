207f0b6879d747b801e7aaf0eb377224b918baaf
nolancoe <99825787+nolancoe@users.noreply.github.com>
2024-01-23 20:41:43 -0600

    typo

--- sweatygames/gunicorn_config.py
+++ sweatygames/gunicorn_config.py
@@ -1,4 +1,4 @@
 workers = 4  # Adjust based on server resources
-bind = "45.33.29.74:8000
+bind = "45.33.29.74:8000"
 chdir = "/etc/var/www/DjangoApp/DjangoApp/sweatygames"
 module = "sweatygames.wsgi:application"
