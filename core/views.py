fbfcc103364609a01a8f6a36eef912b6687a9274
nolancoe <99825787+nolancoe@users.noreply.github.com>
2024-01-25 09:36:52 -0600

    500 view remove

--- core/views.py
+++ core/views.py
@@ -109,8 +109,3 @@ def results_picker(request):
 def permission_denied_view(request):
     raise PermissionDenied
 
-
-def handler500(request, exception, template_name="500.html"):
-    response = render_to_response(template_name)
-    response.status_code = 500
-    return response
